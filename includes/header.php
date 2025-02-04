8091b3c0ca0f1584a852531b9180735ebd1810b2
William Couture <wouture@fsu.edu>
2025-02-03 19:45:28 -0500

    Adds basic index and header for inital testing

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<?php
+    // include all header and navigation details here
+?>
+
+<head>
+    <title><?php echo $page_title ?? "FO Stats" ; ?></title>
+</head>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<?php
+    $root = $_SERVER["DOCUMENT_ROOT"];
+    $page_title = "Home";
+
+    include_once($root . "/includes/header.php");
+?>
+</html>
+

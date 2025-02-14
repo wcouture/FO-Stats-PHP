4eb0ea5e74c5c0042b760b83115786f98bff1290
Wiliam Couture <wpc21@fsu.edu>
2025-02-13 23:58:31 -0500

    Makes player list mobile friendly

--- players/index.php
+++ players/index.php
@@ -2,7 +2,7 @@
     $root = $_SERVER["DOCUMENT_ROOT"];
     $page_title = "Player List";
 
-    $page_css = '<link rel="stylesheet" type="text/css" href="/css/players.css">';
+    $page_css = '<link rel="stylesheet" type="text/css" href="/css/players.css"><link rel="stylesheet" type="text/css" href="/css/games.css">';
 
     include_once $root . "/includes/header.php";
 
@@ -12,8 +12,9 @@
     $db = create_db_connection("faceoff");
     $result = $db->query($sql);
 ?>
-<div class="player-list-container">
+
 <div class="page-title-label">Faceoff Players</div>
+<div class="player-list-container">
   <?php
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
--- players/player-card.php
+++ players/player-card.php
@@ -4,33 +4,20 @@
     $p_number = $row['number'];
     $p_wins = $row['wins'] ?? 0;
     $p_losses = $row['losses'] ?? 0;
+    $p_gbs = $row['gbs'] ?? 0;
     $p_percent = 0;
     if ($p_wins + $p_losses > 0) 
         $p_percent = floatval($p_wins) / (floatval($p_losses) + floatval($p_wins)) * 100;
     $p_gb = $row['gbs'];
 ?>
-<a class="player-card-link" href="/players/view-player?id=<?php echo $p_id;?>">
-<div class="player-card">
-    <!-- <div class="player-link-container">
-        <i class="fa-regular fa-circle-right"></i></a>
-    </div> -->
-    <div class="player-number-label">
-        <?php echo '#'.$p_number; ?>
-    </div>    
-    <div class="player-name-label">
+<a href="/players/view-player?id=<?php echo $p_id; ?>" class="player-performance-card">
+    <div class="performance-game-label">
         <?php echo $p_name; ?>
     </div>
-    <div class="player-wins-label">
-        <?php echo "Wins: ".$p_wins; ?>
+    <div class="performance-stats-container">
+        <div class="performance-wins">Wins: <?php echo $p_wins; ?></div>
+        <div class="performance-losses">Losses: <?php echo $p_losses; ?></div>
+        <div class="performance-gbs">GBs: <?php echo $p_gbs; ?></div>
+        <div class="performance-percent">Win %: <?php echo round($p_percent, 2); ?></div>
     </div>
-    <div class="player-losses-label">
-        <?php echo "Losses: ".$p_losses; ?>
-    </div>
-    <div class="player-percent-label">
-        <?php echo "Win %: ".round($p_percent, 2)."%"; ?>
-    </div>
-    <div class="player-gb-label">
-        <?php echo "GBs: ".$p_gb; ?>
-    </div>
-</div>
 </a>
